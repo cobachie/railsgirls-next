2f42f7ef586da60117fc50b741bca4ef73b68433
cobachie <7006340+cobachie@users.noreply.github.com>
2021-03-08 16:30:44 +0900

    SessionsHelperを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(account: params[:account])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      log_in(user)
       redirect_to ideas_path
     else
       flash[:error] = 'アカウントまたはパスワードが正しくありません。'
