ee5621660a01ada53e2c4e9241e6f99ff51ac695
cobachie <7006340+cobachie@users.noreply.github.com>
2021-03-09 11:06:30 +0900

    未ログインのときログインと一覧画面へのアクセスは可能にする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  before_action :authenticate
+
+  protected
+
+  def authenticate
+    return if logged_in?
+
+    redirect_to login_path
+  end
 end
