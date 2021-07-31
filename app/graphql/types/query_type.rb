571ced935436869977155bcf072382cc7c444afb
cobachie <7006340+cobachie@users.noreply.github.com>
2021-08-01 08:33:53 +0900

    add an entry point to fetch Idea

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :idea, IdeaType, null: true do
+      description "Find a idea by ID"
+      argument :id, ID, required: true
+    end
+
+    def idea(id:)
+      Idea.find(id)
     end
   end
 end
